cd729668fe8548226526d2d5f28f173925b8bc0f
Isaac <isaac.rojas@usach.cl>
2022-01-06 20:43:41 -0300

    FIX - https en el axiosServicio

--- src/service/axiosServicio.js
+++ src/service/axiosServicio.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 const axiosBackend = axios.create({
     //baseURL: 'http://localhost:3632/',
-    baseURL: 'http://isaacrojas-tesis-backend.herokuapp.com/',
+    baseURL: 'https://isaacrojas-tesis-backend.herokuapp.com/',
 });
 
 export default axiosBackend;
