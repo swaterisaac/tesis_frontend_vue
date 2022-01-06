901bcc00463cd8d780a6939d17fce30673613b66
Isaac <isaac.rojas@usach.cl>
2022-01-06 20:30:55 -0300

    ADD - Variables de entorno

--- src/firestoreKey.js
+++ src/firestoreKey.js
@@ -1,11 +1,10 @@
-
 const firebaseConfig = {
-    apiKey: "AIzaSyDWwbgTVWUgXqFOnFxal-geOwOWgO8Q77I",
-    authDomain: "tesis-frontend-vue.firebaseapp.com",
-    projectId: "tesis-frontend-vue",
-    storageBucket: "tesis-frontend-vue.appspot.com",
-    messagingSenderId: "92646019664",
-    appId: "1:92646019664:web:a400c39bd2ed3f6aed7755"
-};
+    apiKey: process.env.API_KEY || "AIzaSyDWwbgTVWUgXqFOnFxal-geOwOWgO8Q77I",
+    authDomain: process.env.API_KEY || "tesis-frontend-vue.firebaseapp.com",
+    projectId: process.env.PROJECT_ID || "tesis-frontend-vue",
+    storageBucket: process.env.STORAGE_BUCKET || "tesis-frontend-vue.appspot.com",
+    messagingSenderId: process.env.MESSAGING_SENDER_ID|| "92646019664",
+    appId: process.env.APP_ID || "1:92646019664:web:a400c39bd2ed3f6aed7755"
+}
 
 export default firebaseConfig;
--- src/service/axiosServicio.js
+++ src/service/axiosServicio.js
@@ -1,7 +1,8 @@
 import axios from 'axios';
 
 const axiosBackend = axios.create({
-    baseURL: 'http://localhost:3632/',
+    //baseURL: 'http://localhost:3632/',
+    baseURL: 'http://isaacrojas-tesis-backend.herokuapp.com/',
 });
 
 export default axiosBackend;
